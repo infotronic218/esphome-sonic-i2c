0d2b4be4abbe1128b0d34e576f83f422416b57a8
infotronic218 <infotronic218@gmail.com>
2023-02-17 07:25:34 +0100

    RFID Card interface integration

--- components/sonic_i2c/sonic_i2c.cpp
+++ components/sonic_i2c/sonic_i2c.cpp
@@ -6,16 +6,9 @@ namespace esphome{
 static const char * TAG = "SONIC SENSOR" ;
 //static SONIC_I2C sensor ;
 namespace sonic_i2c_sensor{
- /*SonicI2C::SonicI2C(){
-  // Init the sensor class 
-  this->sensor = new SONIC_I2C();
-}*/
 
 void SonicI2C::dump_config() {
   LOG_SENSOR("", "Ultrasonic Sensor", this);
-  //ESP_LOGI(TAG," SDA Pin: %d", this->sda_pin_);
-  //ESP_LOGI(TAG," SCL Pin: %d", this->scl_pin_);
-  //ESP_LOGCONFIG(TAG, "  Timeout: %u µs", this->timeout_us_);
   LOG_UPDATE_INTERVAL(this);
 }
 
@@ -24,22 +17,11 @@ float SonicI2C::getDistance(){
     uint32_t data;
     uint8_t val = 0x01 ;
     this->write( &val, 1);
-    //_wire->beginTransmission(_addr);  // Transfer data to 0x57. 将数据传输到0x57
-    //_wire->write(0x01);
-    //_wire->endTransmission();  // Stop data transmission with the Ultrasonic
-                               // Unit. 停止与Ultrasonic Unit的数据传输
+    
     delay(120);
     uint8_t data_buffer[]={0,0,0,0,0};
     
     this->read(data_buffer,3);
-    //_wire->requestFrom(_addr,
-    //                   (uint8_t)3);  // Request 3 bytes from Ultrasonic Unit.
-                                     // 向Ultrasonic Unit请求3个字节。
-    //data = _wire->read();
-   // data <<= 8;
-    //data |= _wire->read();
-    //data <<= 8;
-    //data |= _wire->read();
     data = data_buffer[0]<< 16 | data_buffer[1]<< 8 | data_buffer[2];
     float Distance = float(data) / 1000;
     if (Distance > 4500.00) {
@@ -53,8 +35,6 @@ float SonicI2C::getDistance(){
 void SonicI2C::setup(){
     // Init the sensor 
     ESP_LOGI(TAG, "Sonic Sensor Setup begin");
-    //this->sensor = new SONIC_I2C();
-    //this->sensor->begin(&Wire, 0x57, this->sda_pin_,this->scl_pin_);
     
 }
 
--- components/sonic_i2c/sonic_i2c.h
+++ components/sonic_i2c/sonic_i2c.h
@@ -13,13 +13,9 @@ namespace sonic_i2c_sensor{
 
 class SonicI2C: public i2c::I2CDevice, public sensor::Sensor, public PollingComponent {
     private:
-    //uint8_t sda_pin_;
-    //uint8_t scl_pin_;;
-    //SONIC_I2C *sensor ;
+
     public:
     
-     //void set_sda_pin(uint8_t sda_pin) { sda_pin_ = sda_pin; }
-     //void set_scl_pin(uint8_t scl_pin) { scl_pin_ = scl_pin; }
      void setup() override ;
      void dump_config() override;
      void update() override ;
